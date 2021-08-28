9f29622e9f491232699db0ea7a2c1101a0821623
Caleb S. Lipan <46553773+caleblipan@users.noreply.github.com>
2021-08-28 14:25:21 +0700

    added delete technicians functionality

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const TechItem = ({ tech }) => {
-	return (
-		<li className="collection-item">
-			<div>
-				{tech.firstName} {tech.lastName}
-				<a href="#!" className="secondary-content">
-					<i className="material-icons grey-text">delete</i>
-				</a>
-			</div>
-		</li>
-	)
-}
+import { deleteTech } from "../../actions/techActions";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const TechItem = ({ tech, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(tech.id);
+    M.toast({ html: "Technician Removed" });
+  };
+  return (
+    <li className="collection-item">
+      <div>
+        {tech.firstName} {tech.lastName}
+        <a href="#!" className="secondary-content" onClick={onDelete}>
+          <i className="material-icons grey-text">delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
 
 TechItem.propTypes = {
-	tech: PropTypes.object.isRequired
-}
+  tech: PropTypes.object.isRequired
+};
 
-export default TechItem
+export default connect(null, { deleteTech })(TechItem);
